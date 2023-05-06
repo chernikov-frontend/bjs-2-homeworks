d21fccc5e61ba066a27788b58999ea086ab1a9e1
Chernikov S.V <chernikov-frontend@yandex.ru>
2023-05-06 15:54:18 +0400

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
-function Student(name, gender, age) {
-  
+function Student(name, gender, age) { 
+    this.name = name,
+    this.gender = gender,
+    this.age = age,
+    this.marks = []
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (!(this.hasOwnProperty('marks'))) {
+        return "Невозможность добавлять оценки отчисленному студенту";
+    } else if (this.marks === undefined) {
+        this.marks = marks;
+    } else {
+        this.marks = this.marks.concat(marks);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.marks === undefined || isNaN(...this.marks)) {
+        return 0;
+    } else {
+        return this.marks.reduce((acc, item) => acc + item, 0) / this.marks.length;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+
+    this.excluded = reason;
 }
