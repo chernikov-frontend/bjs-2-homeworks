ff4ca7900ab75b44226bff5f1ef23bc7de1984fa
Chernikov S.V <chernikov-frontend@yandex.ru>
2023-05-17 14:02:08 +0400

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,49 @@
-﻿
+﻿// Задача 1. Форматтер чисел
+
+function parseCount(unit) {
+    let unitConverted = Number.parseFloat(unit)
+    if (isNaN(unitConverted)) {
+        throw new Error("Невалидное значение");
+    }
+    return unitConverted;
+}
+
+function validateCount(unit) {
+    try {
+        return parseCount(unit);
+    } catch (error) {
+        return error;
+    }
+}
+
+// Задача 2. Треугольник
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a; 
+        this.b = b; 
+        this.c = c;
+        if (a + b < c || a + c < b || b + c < a) throw new Error('Треугольник с такими сторонами не существует');
+    }
+
+    get perimeter() {
+        return (this.a + this.b + this.c);
+    }
+
+    get area() {
+    let p = (this.a + this.b + this.c) * 0.5; 
+    let s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+    return +s.toFixed(3);
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get perimeter() { return "Ошибка! Треугольник не существует" },
+            get area() { return "Ошибка! Треугольник не существует" }
+        }
+    }
+}
